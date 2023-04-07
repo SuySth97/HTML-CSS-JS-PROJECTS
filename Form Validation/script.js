31adb537f10f36ecf940ac3677a3e54e6d4e5f4f
Suyog Shrestha <suyogshrestha97@gmail.com>
2023-04-07 09:39:25 +0545

    push

--- Form Validation/script.js
+++ Form Validation/script.js
@@ -1,15 +1,53 @@
 let user = document.getElementById("username");
+let userError = document.getElementById("user-error");
+let email = document.getElementById("email");
+let emailError = document.getElementById("email-error");
 let pass = document.getElementById("password");
+let passError = document.getElementById("pass-error");
+let flag = 1;
 
-function validateForm(){
-  if(user.value == ""){
-    document.getElementById("user-error").innerHTML = "Username is empty!!!";
-  }else if(user.value.length < 3){
-    document.getElementById("user-error").innerHTML = "Username require mininum 3 characters";
+function validateForm() {
+  if (user.value == "") {
+    userError.innerHTML = "Username is empty!!!";
+    userError.style.border = "1px solid red";
+    userError.style.display = "block";
+    user.focus();
+    flag = 0;
+  } else if (user.value.length < 3) {
+    userError.innerHTML = "Username require min 3 char";
+    flag = 0;
+  } else {
+    document.getElementById("user-error").innerHTML = "";
+    flag = 1;
   }
 
-  if(pass.value == ""){
-    document.getElementById("pass-error").innerHTML = "Password is empty!!!";
+  if (email.value == "") {
+    emailError.innerHTML = "Provide your email address";
+    emailError.style.border = "1px solid red";
+    emailError.style.display = "block";
+    email.focus();
+    flag = 0;
+  } else {
+    emailError.innerHTML = "";
+    flag = 1;
+  }
+
+  if (pass.value == "") {
+    passError.innerHTML = "Password is empty!!!";
+    passError.style.border = "1px solid red";
+    passError.style.display = "block";
+    pass.focus();
+    flag = 0;
+  } else {
+    passError.innerHTML = "";
+    flag = 1;
+  }
+  
+  // if validation is satisfied
+  if(flag){
+    return true;
+  }else{
+    return false;
   }
 }
 
