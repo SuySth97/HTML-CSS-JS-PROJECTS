44bc81c1cb4c349227d2c4bd054e646ec80d38aa
Suyog Shrestha <suyogshrestha97@gmail.com>
2023-04-07 10:58:41 +0545

    new

---
+++ Image Slider/script.js
@@ -0,0 +1,36 @@
+// images = 4(0,1,2,3)
+
+let flag = 0;//to match with array[2]
+
+
+function controller(x) {
+  flag = flag + x;
+  slideShow(flag);
+  //left click
+  // flag = 2 + (-1) = 1
+  //right click
+  // flag = 2 + (+1) = 3
+}
+
+slideShow(flag);
+
+function slideShow(num) {
+  let slides = document.getElementsByClassName("slide");
+
+  if (num == slides.length){
+    flag = 0;
+    num = 0;
+  }
+
+  if(num < 0){
+    flag = slides.length-1;
+    num = slides.length-1;
+  }
+
+  for (let y of slides) {
+    y.style.display = "none";
+  }
+
+  slides[num].style.display = "block";//display image at array[2] 
+
+}
